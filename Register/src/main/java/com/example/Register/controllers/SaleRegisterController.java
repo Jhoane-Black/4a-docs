93c7c5795490c070f1209fd0a09c692d1c9cf66d
dantiteis <dantiteis@gmail.com>
2021-11-27 13:01:21 -0500

    exceptions added

--- Register/src/main/java/com/example/Register/controllers/SaleRegisterController.java
+++ Register/src/main/java/com/example/Register/controllers/SaleRegisterController.java
@@ -1,10 +1,12 @@
 package com.example.Register.controllers;
 
+import com.example.Register.exceptions.SaleRegisterNotFoundException;
 import com.example.Register.repositories.SaleRegisterRepository;
 import com.example.Register.models.SaleRegister;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 @RestController
 public class SaleRegisterController {
@@ -16,7 +18,26 @@ public class SaleRegisterController {
 
     @GetMapping("/saleregisters/{nombrecliente}") // convert the next method to an endpoint
     List<SaleRegister> getSaleRegisterByCliente(@PathVariable String nombrecliente){
-        return this.repository.getByNombreCliente(nombrecliente);
+        List<SaleRegister> saleRegister = repository.getByNombreCliente(nombrecliente);
+        int corroborate = 0;
+        int corroborated = 0;
+        for(SaleRegister i : saleRegister){
+            if(i.getNombreCliente().equals(nombrecliente)){
+                corroborate = 0;
+                corroborated = 1;
+            }
+            else{
+                if (corroborated == 0){
+                    corroborate = 1;
+                }
+            }
+        }
+        if (corroborate == 0){
+            return this.repository.getByNombreCliente(nombrecliente);
+        }
+        else{
+            throw new SaleRegisterNotFoundException("El cliente no existe");
+        }
     }
 
     @GetMapping("/saleregistersc/{proveedor}/{nombrecliente}") // convert the next method to an endpoint
@@ -46,9 +67,9 @@ public class SaleRegisterController {
         SaleRegister oldSRegister = repository.findById(registroID).orElse(null);
         oldSRegister.setCantidad(newSRegister.getCantidad());
         oldSRegister.setFecha(newSRegister.getFecha());
-        oldSRegister.setNombre_cliente(newSRegister.getNombre_cliente());
+        oldSRegister.setNombreCliente(newSRegister.getNombreCliente());
         oldSRegister.setProveedor(newSRegister.getProveedor());
-        oldSRegister.setNombre_producto(newSRegister.getNombre_producto());
+        oldSRegister.setNombreProducto(newSRegister.getNombreProducto());
         oldSRegister.setPromocion(newSRegister.getPromocion());
         oldSRegister.setPrecio(newSRegister.getPrecio());
         oldSRegister.setTotal(newSRegister.getPrecio(),newSRegister.getCantidad());
